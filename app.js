be347a293d89396922ee2046a29e49a688e59d02
malaikaandrade <malaikaandrade@gmail.com>
2021-07-13 19:01:22 -0500

    agregue un callback

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Hola mundo");
+function ejercicioClase(callback1, callback2, callback3){
+    callback1();
+    callback2();
+    callback3();
+}
+
+function gatito(){
+    console.log('soy un gatito');
+}
+function perrito(){
+    console.log('soy un perrito');
+}
+function monito(){
+    console.log('soy un monito');
+}
+ejercicioClase(gatito, perrito, monito);
+
